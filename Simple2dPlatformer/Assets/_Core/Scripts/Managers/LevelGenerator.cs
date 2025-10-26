465ba91ee9ad5b3302511c5ea6be61f91a44ac94
Pavel Halkovsky <pasha.galkovsky22@gmail.com>
2025-10-26 09:13:37 +0200

    Update LevelGenerator.cs

--- Simple2dPlatformer/Assets/_Core/Scripts/Managers/LevelGenerator.cs
+++ Simple2dPlatformer/Assets/_Core/Scripts/Managers/LevelGenerator.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Core.Scripts.Managers
 {
     public class LevelGenerator : MonoBehaviour
     {
-        [SerializeField] private List<GameObject> platformPrefab;
-        private List<GameObject> spawnedObjects;
+        [SerializeField] private List<GameObject> platformPrefabToSpawn;
+        [SerializeField] private GameObject startedPlatform;
+        private Queue<GameObject> spawnedObjects;
         private int offsetToSpawn = 1;
-        private int maxAmountOfPlatforms = 15;
+        private int maxAmountOfPlatforms = 5;
         GameManager gameManager;
 
         private void Start()
         {
             gameManager = GameManager.Instance;
+            spawnedObjects = new Queue<GameObject>();
+            spawnedObjects.Enqueue(startedPlatform);
+            
             StartCoroutine(PlatformGenerator());
         }
 
@@ -24,12 +29,16 @@ namespace _Core.Scripts.Managers
         {
             while (true)
             {
-                int randomIndex = Random.Range(0, platformPrefab.Count);
-                GameObject platform = Instantiate(platformPrefab[randomIndex], new Vector2(Random.Range(-6.5f, 6.5f), 9f + offsetToSpawn), Quaternion.identity);
+                int randomIndex = Random.Range(0, platformPrefabToSpawn.Count);
+                GameObject platform = Instantiate(platformPrefabToSpawn[randomIndex], new Vector2(Random.Range(-6.5f, 6.5f), 9f + offsetToSpawn), Quaternion.identity);
                 offsetToSpawn += 2;
-                spawnedObjects.Add(platform);
-                if (spawnedObjects.Count > maxAmountOfPlatforms)
+                
+                spawnedObjects.Enqueue(platform);
+                if (spawnedObjects.Count >= maxAmountOfPlatforms)
                 {
+                    Debug.Log("Max amount of platforms reached");
+                    Destroy(spawnedObjects.Peek());
+                    spawnedObjects.Dequeue();
                     
                 }
                 yield return new WaitForSeconds(1f);
